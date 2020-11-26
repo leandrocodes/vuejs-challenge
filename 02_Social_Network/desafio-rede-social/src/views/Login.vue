a9dcfdfc7b4efd5f2aff9c5c6507b4fe9384da84
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 16:03:56 -0400

    :construction: login form

---
+++ 02_Social_Network/desafio-rede-social/src/views/Login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login__page">
+    <h1>Login</h1>
+    <form class="login__form d-flex fd-column" @submit="login">
+      <label for="email-input">Email: </label>
+      <input id="email-input" type="email" placeholder="joao@email.com" />
+
+      <label for="pass-input">Senha: </label>
+      <input id="pass-input" type="password" placeholder="******" />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.login__page {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    to right top,
+    #f39237,
+    #f27848,
+    #ea6159,
+    #d94e69,
+    #c14377,
+    #a94482,
+    #8d4789,
+    #6f4989,
+    #524d84,
+    #3b4e79,
+    #2f4c69,
+    #2f4858
+  );
+
+  h1 {
+    font-weight: 300;
+    color: white;
+    font-size: 4rem;
+    text-align: center;
+  }
+
+  .login__form {
+    background: #ffeacf1a;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    width: 80%;
+    margin: 0 auto;
+    padding: 1rem 1rem;
+    box-shadow: 0 12.5px 10px rgba(0, 0, 0, 0.035),
+      0 100px 80px rgba(0, 0, 0, 0.07);
+    border-radius: 8px;
+    transform: translateY(-50%);
+    label {
+      color: white;
+      font-size: 1.5rem;
+      font-weight: 300;
+      &:last-of-type {
+        margin-top: 1.5rem;
+      }
+    }
+    input {
+      height: 45px;
+      padding: 0 0.5rem;
+      border-radius: 4px;
+      background-color: white;
+      border: 2px solid white;
+      &::placeholder {
+        color: black;
+      }
+      &:focus {
+        outline: none;
+      }
+      &:-webkit-autofill {
+        box-shadow: 0 0 0 30px white inset !important;
+      }
+    }
+
+    button {
+      margin-top: 1.5rem;
+      margin-left: auto;
+      width: 50%;
+      height: 45px;
+      background-color: #008047;
+      border: 1px solid transparent;
+      border-radius: 4px;
+      color: white;
+      font-weight: 500;
+      box-shadow: 0 12.5px 10px rgba(0, 0, 0, 0.05),
+        0 100px 80px rgba(0, 0, 0, 0.1);
+    }
+  }
+}
+</style>
