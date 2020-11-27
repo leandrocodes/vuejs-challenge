7b4f5018eedb00067c11b0a160b8a54bcd3487a5
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 21:56:48 -0400

    :construction: renderizando posts e adicionando comentário aos posts

--- 02_Social_Network/desafio-rede-social/src/views/Feed.vue
+++ 02_Social_Network/desafio-rede-social/src/views/Feed.vue
@@ -2,14 +2,34 @@
   <div class="feed">
     <navbar></navbar>
     <h2 class="fw-normal ta-center">Feed de Postagens</h2>
+
+    <div class="posts d-flex fd-column ai-center">
+      <post
+        v-for="(post, index) in posts"
+        :post="post"
+        :key="post.id"
+        @comment="addComment($event, index)"
+      ></post>
+    </div>
   </div>
 </template>
 
 <script>
 import navbar from '@/components/navbar'
+import post from '@/components/post.vue'
+import posts from '../posts'
 export default {
   components: {
-    navbar
+    navbar,
+    post
+  },
+  data: () => ({
+    posts
+  }),
+  methods: {
+    addComment(comment, index) {
+      this.posts[index].comments.push(comment)
+    }
   }
 }
 </script>
