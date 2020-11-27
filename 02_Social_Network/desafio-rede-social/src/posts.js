f9bf98f9925768d44385d0cd07b5463ff3e2fb95
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 21:55:18 -0400

    :fire: removendo arquivos não utilizados

--- 02_Social_Network/desafio-rede-social/src/posts.js
+++ 02_Social_Network/desafio-rede-social/src/posts.js
@@ -3,7 +3,6 @@ export default [
     id: 1,
     owner: 'John Doe',
     description: 'A really nice post',
-    likes: 12,
     timestamp: '03:42 am',
     comments: [{ owner: 'mariaelza', text: 'nice picture!' }],
     image: require('@/assets/images/post.jpg')
@@ -12,7 +11,6 @@ export default [
     id: 2,
     owner: 'Maria Elza',
     description: 'Another really nice post',
-    likes: 6,
     timestamp: '07:32 pm',
     comments: [{ owner: 'ferdinand', text: 'This picture is awesome!' }],
     image: require('@/assets/images/post.jpg')
