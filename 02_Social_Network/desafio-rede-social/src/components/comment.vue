efbddca0e6c786d082efb4fd01d3a43dfead0661
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 21:56:13 -0400

    componente de comentários

---
+++ 02_Social_Network/desafio-rede-social/src/components/comment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="comment d-flex">
+    <p class="comment__owner">{{ comment.owner }}:</p>
+    <p class="comment__text fw-normal">{{ comment.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.comment {
+  .comment__text {
+    margin-left: 4px;
+  }
+}
+</style>
