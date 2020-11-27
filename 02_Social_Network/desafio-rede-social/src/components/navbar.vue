5f204f9ef322d484b961e959c372ab1e0710a131
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 20:27:51 -0400

    :construction: :art: componente de navbar e feed

---
+++ 02_Social_Network/desafio-rede-social/src/components/navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar">
+    <div class="profile">
+      <img src="../assets/images/profile.jpg" alt="Profile Picture" />
+    </div>
+    <div class="profile__name">
+      <span>John Doe</span>
+    </div>
+
+    <div class="navbar__actions">
+      <router-link to="/">
+        <img src="../assets/icons/log-out.svg" alt="" />
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100%;
+  height: 65px;
+  background-color: $primary;
+  display: flex;
+  align-items: center;
+  padding: 0 1.25rem;
+  box-shadow: $box-shadow-sm;
+
+  .profile {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 50%;
+    }
+  }
+
+  .profile__name {
+    color: white;
+    margin-left: 1rem;
+    font-size: 1.25rem;
+  }
+
+  .navbar__actions {
+    margin-left: auto;
+  }
+}
+</style>
---
+++ 02_Social_Network/desafio-rede-social/src/views/Feed.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="feed">
+    <navbar></navbar>
+    <h2 class="fw-normal ta-center">Feed de Postagens</h2>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar'
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.feed {
+  h2 {
+    margin: 1rem 0;
+    color: $primary;
+  }
+}
+</style>
