61902af4b20d808b366e0980788b401b834646a6
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 22:28:44 -0400

    :construction: update mock

--- 02_Social_Network/desafio-rede-social/src/posts.js
+++ 02_Social_Network/desafio-rede-social/src/posts.js
@@ -13,6 +13,14 @@ export default [
     description: 'Another really nice post',
     timestamp: '07:32 pm',
     comments: [{ owner: 'ferdinand', text: 'This picture is awesome!' }],
-    image: require('@/assets/images/post.jpg')
+    image: require('@/assets/images/norway.jpg')
+  },
+  {
+    id: 3,
+    owner: 'Zack Stewart',
+    description: 'Another really nice post',
+    timestamp: '06:02 am',
+    comments: [{ owner: 'ferdinand', text: 'This picture is awesome!' }],
+    image: require('@/assets/images/boreal.jpg')
   }
 ]
