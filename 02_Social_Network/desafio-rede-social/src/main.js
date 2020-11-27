98e9b765f0ea5db6116c011e0b8e7d758951c69e
leandrocodes <leandrovianacodes@gmail.com>
2020-11-26 22:21:59 -0400

    :fire: removendo vuex

--- 02_Social_Network/desafio-rede-social/src/main.js
+++ 02_Social_Network/desafio-rede-social/src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 import './style/main.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
